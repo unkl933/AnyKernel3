8923b8c1389ee99518d2daefeb22afe8667a001d
Jonas Cardoso <jonascard60@gmail.com>
2020-01-06 21:31:07 -0300

    anykernel.sh: This is FloppyKernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=FloppyKernel by JonasCardoso @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
